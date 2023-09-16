9a199cf89a7447b8d8562853968ea90ab0bffad6
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-09-16 06:20:30 +0000

    [update&finish]応用課題4

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-    resources :users, only: [:index, :show, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+        get "follows" => "relationships#follows", as: "follows"
+        get "followers" => "relationships#followers", as: "followers"
+    end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
