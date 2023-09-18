d5622a54b4273743cd02f843335f8796dbd25eba
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-09-18 05:05:29 +0000

    [update]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,13 +4,11 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
   end
 
   def destroy
     comment = BookComment.find(params[:id])
     comment.destroy
-    redirect_to request.referer
   end
 
   private
