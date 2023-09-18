bef5aeaf7f375602d8b4945333dd6ecc35b65b87
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-09-18 04:14:28 +0000

    [update]応用課題6

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
       get "follows" => "relationships#follows", as: "follows"
